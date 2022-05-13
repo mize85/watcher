7da24ccb8d91b2bf38b5d03979e523443c84df3c
larsbunting <lars.buenting@smacc.io>
2022-05-13 17:00:35 +0200

    small changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const XLSX = require('xlsx');
 const path = require('path');
 const xml2js = require('xml2js');
 const fs = require('fs');
+const builder = new xml2js.Builder();
 
 // Initialize watcher.
 const watcher = chokidar.watch('input', {
@@ -18,18 +19,15 @@ const log = console.log.bind(console);
 watcher.on('add', handleNewFile)
 
 function handleNewFile(filePath) {
-  log(`File ${path} has been added`);
+  log(`File ${filePath} has been added`);
 
-  const ext = path.extname(filePath)
+  const fileName = path.basename(filePath);
 
-  // we only care about excel files
-  if (ext === ".xlsx") {
+  // we only care about userliste files
+  if (fileName === "userliste.xlsx") {
     const workbook = XLSX.readFile(filePath);
     const sheet_name_list = workbook.SheetNames;
     const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    const builder = new xml2js.Builder();
-
-    // <user role="administrator" identifier="0000000000101" name="Administrator" password="12Ev3CEYljHE." login="1234"/>
     const xml = builder.buildObject({
       $: {serverId: "", version: ""}, users: xlData.map(d => ({
         $: {role: d.role, identifier: d.login_1, name: d.name, password: d.password, login: d.login}
